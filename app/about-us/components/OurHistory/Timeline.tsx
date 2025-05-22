fd492f33588018c9da18d5666262312b7e456be7
Alejandro Bermudez <alejandro@example.com>
2025-05-22 13:11:47 -0500

    better large timeline

--- app/about-us/components/OurHistory/Timeline.tsx
+++ app/about-us/components/OurHistory/Timeline.tsx
@@ -16,41 +16,44 @@ const Timeline = () => {
     flip?: boolean;
   }) => (
     <div
-      className={`h-[300px] flex ${
+      className={`h-[380px] flex ${
         flip ? "flex-col-reverse" : "flex-col"
       } justify-between items-center`}
     >
-      <div className="p-1 bg-white  w-[170px] relative h-[112px]">
-        <div className="border-2 p-1 h-full border-[#369C2A]">
-          <div className="flex items-center justify-between gap-1 ">
-            <p className="text-3xl font-montserrat text-muted-foreground">
+      <div className="p-2 bg-white w-[220px] relative h-[140px]">
+        <div className="border-2 p-3 h-full border-[#369C2A]">
+          <div className="flex items-center justify-between gap-2 mb-0">
+            <p className="text-4xl font-montserrat text-muted-foreground font-bold">
               {year}
-            </p>{" "}
-            <div className="px-3 py-1 bg-stone-900 text-xs flex flex-col justify-center items-center text-white">
+            </p>
+            <div className="px-3 py-2 bg-stone-800 text-sm flex flex-col justify-center items-center text-white">
               Event {eventNumber}
             </div>
           </div>
-          <p className="text-muted-foreground leading-[1] flex flex-col justify-center  h-[60px] text-xs text-center">
+          <p className="text-muted-foreground leading-4 flex flex-col justify-center h-[50px] text-sm text-center">
             {text}
           </p>
         </div>
-        <Quote className="absolute top-[-10] left-[-10] bg-white rounded-md p-1 text-[#369C2A]" />
-        <Quote className="absolute bottom-[-10] right-[-10] bg-white rounded-md p-1 text-[#369C2A]" />
+        <Quote className="absolute top-[-12px] left-[-12px] bg-white rounded-md p-2 text-[#369C2A] w-7 h-7" />
+        <Quote className="absolute bottom-[-12px] right-[-12px] bg-white rounded-md p-2 text-[#369C2A] w-7 h-7" />
         {flip ? (
-          <ChevronDown className="absolute  rounded-full text-[#369C2A] left-[70px] top-[-30]" />
+          <ChevronDown className="absolute rounded-full text-[#369C2A] left-[90px] top-[-35px] w-7 h-7" />
         ) : (
-          <ChevronUp className="absolute  rounded-full  text-[#369C2A]  left-[70px] bottom-[-30]" />
+          <ChevronUp className="absolute rounded-full text-[#369C2A] left-[90px] bottom-[-35px] w-7 h-7" />
         )}
       </div>
       <div>
-        <img className="aspect-video object-fill w-[180px]" src={img} />
+        <img
+          className="aspect-video object-cover w-[240px] h-[135px]"
+          src={img}
+        />
       </div>
     </div>
   );
 
   return (
     <div className="hidden relative xl:block">
-      <div className="grid grid-cols-6 gap-6 ">
+      <div className="grid grid-cols-6 gap-8">
         <TimeLineTextBox
           year="2009"
           eventNumber="1"
@@ -80,15 +83,13 @@ const Timeline = () => {
         <TimeLineTextBox
           year="2023"
           eventNumber="5"
-          text="Operations expanded to 75 employees, with 
-            6 department heads."
+          text="Operations expanded to 75 employees, with 6 department heads."
           img={"/images/internal/leaders/songgongbo.jpg"}
         />
         <TimeLineTextBox
           year="2025"
           eventNumber="6"
-          text="Over 100 employees, 10 departments, 9 managers, & growth rate 
-up to 25%-30%."
+          text="Over 100 employees, 10 departments, 9 managers, & growth rate up to 25%-30%."
           img={"/images/internal/leaders/songgongbo.jpg"}
           flip
         />
