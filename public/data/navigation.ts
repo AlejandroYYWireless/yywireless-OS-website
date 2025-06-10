de14c11e9beeacd3957f6023b9081513835d4a0e
AlejandroYYWireless <alejandro@yywireless.com>
2025-06-10 12:49:13 -0500

    reordered page

--- public/data/navigation.ts
+++ public/data/navigation.ts
@@ -4,18 +4,9 @@ export const navigation = [
       href: "/",
       label: "Home",
     },
-
-    "about-us": {
-      href: "/about-us",
-      label: "About Us",
-    },
     services: {
       href: "/services",
       label: "Services",
-    },
-    "contact-us": {
-      href: "/contact-us",
-      label: "Contact Us",
     },
     wholesale: {
       href: "/wholesale",
@@ -24,6 +15,15 @@ export const navigation = [
     retail: {
       href: "/retail",
       label: "Retail",
+    },
+    "about-us": {
+      href: "/about-us",
+      label: "About Us",
+    },
+
+    "contact-us": {
+      href: "/contact-us",
+      label: "Contact Us",
     },
   },
 ];
