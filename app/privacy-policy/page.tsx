756f2883baf60f4fa1145ffdde36f0106191f05d
Alejandro Bermudez <alejandro@example.com>
2025-09-10 12:39:14 -0500

    flex start

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -364,7 +364,7 @@ export default function PrivacyPolicyPage() {
                   us at:
                 </p>
                 <div className="grid md:grid-cols-2  gap-4">
-                  <div className="flex items-center space-x-3 p-3 border rounded-lg">
+                  <div className="flex items-start space-x-3 p-3 border rounded-lg">
                     <Mail className="w-5 h-5 text-stone-200 flex-shrink-0" />
                     <div>
                       <p className="font-medium text-sm">Email</p>
